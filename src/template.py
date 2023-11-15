5cbdb9e58e05b65e017cd4f3626f2ac6470a137a
Noob_daidai <56266769+0xdaidai@users.noreply.github.com>
2023-11-15 09:18:41 +0800

    Update template.py

--- src/template.py
+++ src/template.py
@@ -1,30 +1,38 @@
 #!/usr/bin/env python3
 
 from pwn import *
+import sys
+context.log_level = "debug"
+context.binary = {bin_name}
 
 {bindings}
 
-context.binary = {bin_name}
+if len(sys.argv) < 2:
+    debug = True
+else:
+    debug = False
 
+if debug:
+    p = process({proc_args})
+    libc = ELF("/lib/x86_64-linux-gnu/libc.so.6")
+else:
+    p = remote("addr", 1337)
+    libc = ELF("./libc-2.31.so")
 
-def conn():
-    if args.LOCAL:
-        r = process({proc_args})
-        if args.DEBUG:
-            gdb.attach(r)
-    else:
-        r = remote("addr", 1337)
+ru = lambda x : p.recvuntil(x)
+sn = lambda x : p.send(x)
+rl = lambda : p.recvline()
+sl = lambda x : p.sendline(x)
+rv = lambda x : p.recv(x)
+sa = lambda a,b : p.sendafter(a,b)
+sla = lambda a,b : p.sendlineafter(a, b)
 
-    return r
+def debugf(b=0):
+    if debug:
+        if b:
+            gdb.attach(p,"b *$rebase({b})".format(b = hex(b)))
+        else:
+            gdb.attach(p)
+#context.terminal = ['tmux', 'splitw', '-h']
 
-
-def main():
-    r = conn()
-
-    # good luck pwning :)
-
-    r.interactive()
-
-
-if __name__ == "__main__":
-    main()
+p.interactive()
